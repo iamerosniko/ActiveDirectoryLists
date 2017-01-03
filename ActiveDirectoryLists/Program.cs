4bcc1271dfe9ad9dd2c99fcf8cc3dc06438c8524
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-01-03 16:19:46 -0500

    add

--- ActiveDirectoryLists/Program.cs
+++ ActiveDirectoryLists/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.DirectoryServices;
-using System.DirectoryServices.AccountManagement;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
