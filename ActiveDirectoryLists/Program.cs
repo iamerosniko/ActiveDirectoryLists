add6d2a5995e4363e1118e49e2d93841f851395b
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-01-03 08:24:45 -0500

    Fetch All Users
    GetAllUsers Requires a Valid Domain Name

--- ActiveDirectoryLists/Program.cs
+++ ActiveDirectoryLists/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ActiveDirectoryLists
 {
@@ -10,6 +13,29 @@ namespace ActiveDirectoryLists
     {
         static void Main(string[] args)
         {
+            GetAllUsers("");
+
+            
         }
+        static void GetAllUsers(string yourDomainName)
+        {
+            using (var context = new PrincipalContext(ContextType.Domain, yourDomainName))
+            {
+                using (var searcher = new PrincipalSearcher(new UserPrincipal(context)))
+                {
+                    foreach (var result in searcher.FindAll())
+                    {
+                        DirectoryEntry de = result.GetUnderlyingObject() as DirectoryEntry;
+                        Console.WriteLine("First Name: " + de.Properties["givenName"].Value);
+                        Console.WriteLine("Last Name : " + de.Properties["sn"].Value);
+                        Console.WriteLine("SAM account name   : " + de.Properties["samAccountName"].Value);
+                        Console.WriteLine("User principal name: " + de.Properties["userPrincipalName"].Value);
+                        Console.WriteLine();
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+
     }
 }
