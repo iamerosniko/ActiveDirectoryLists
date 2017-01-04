99423fc4bc9edbe6d50e9bf0bcaa7a11763170e7
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-01-04 07:58:35 -0500

    up

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static ACLQuery acl = new ACLQuery();
+        static ACLEntities aclEntity = new ACLEntities();
+        static void Main(string[] args)
+        {
+            sampleGetUsers();
+            Console.Read();
+        }
+
+        //using GetUsers
+        static void sampleGetUsers()
+        {
+            string myDomainName = ConsoleReadAndWrite("Input Domain Name: ");
+            string searchString = ConsoleReadAndWrite("Search by username: ");
+
+            acl.GetConnectToDomain(myDomainName);
+
+            foreach (var a in acl.getUsers(searchString))
+            {
+                Console.WriteLine(a.ACL_DisplayName);
+            }
+        }
+
+        public static string ConsoleReadAndWrite(string instruction)
+        {
+            Console.Write(instruction);
+            return Console.ReadLine();
+        }
+
+    }
+}
