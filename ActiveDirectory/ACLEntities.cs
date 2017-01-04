095222542cc4c488005656022ecbbc9a21b4c0fa
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-01-04 08:36:30 -0500

    .

--- ActiveDirectory/ACLEntities.cs
+++ ActiveDirectory/ACLEntities.cs
@@ -9,9 +9,9 @@ namespace ActiveDirectory
     public class ACLEntities
     {
         public string ACL_UserName { get; set; }
-        public string ACL_GivenName { get; set; }
-        public string ACL_Surname { get; set; }
-        public string ACL_Principalname { get; set; }
+        //public string ACL_GivenName { get; set; }
+        //public string ACL_Surname { get; set; }
+        //public string ACL_Principalname { get; set; }
         public string ACL_DisplayName { get; set; }
     }
 }
